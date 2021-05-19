e2dcef312ee61216802e8303f6546dfbb110dcd4
singhsachindra1996 <singh.sachindra1996@gmail.com>
2021-05-19 11:55:58 +0530

    set live config

--- dbConnection.js
+++ dbConnection.js
@@ -1,8 +1,22 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host:'localhost',
-    user: 'root',
-    password: '',
-    database:'demo'
+    host:'remotemysql.com',
+    user: 'XXmGizzbW7',
+    password: 'bYJSebS5C4',
+    database:'XXmGizzbW7'
 });
 module.exports=connection;
+
+// You have successfully created a new database. The details are below.
+
+// Username: XXmGizzbW7
+
+// Database name: XXmGizzbW7
+
+// Password: bYJSebS5C4
+
+// Server: remotemysql.com
+
+// Port: 3306
+
+// These are the username and password to log in to your database and phpMyAdmin
